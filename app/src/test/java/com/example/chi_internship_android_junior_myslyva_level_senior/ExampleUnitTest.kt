bbb05362ab1b38fc81dd23a4db1629e7b84d23be
Yaroslava Myslyva <yaroslava.met@gmail.com>
2022-09-13 17:41:27 +0300

    Initial commit

---
+++ app/src/test/java/com/example/chi_internship_android_junior_myslyva_level_senior/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.chi_internship_android_junior_myslyva_level_senior
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
